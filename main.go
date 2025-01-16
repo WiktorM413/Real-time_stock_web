b5e7ecf7432ebde76f3247c32b769580ca1d3bd7
Wiktor M <wiktor.markowski81@gmail.com>
2025-01-16 18:20:47 +0100

    Some additions

--- main.go
+++ main.go
@@ -2,9 +2,18 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	fmt.Println("http://localhost:8080")
+	router := gin.Default()
 
-	
+	router.LoadHTMLGlob("templates/*")
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{"title": "Home page"})
+	})
+	router.Run()
 }
